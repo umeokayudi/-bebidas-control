6f5a3e83d2b1217eb53259463fe449c7d10f0810
Augie1316 <swamyee09@gmail.com>
2026-06-08 10:26:07 +0900

    feat: restore all tabs + cashflow

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import PortalCliente from './components/PortalCliente'
 import { ProductsTab, BarsTab, UsuariosTab } from './components/Configs'
 import Fornecedores from './components/Fornecedores'
 import Faturas from './components/Faturas'
+import Cashflow from './components/Cashflow'
 import AIAssistant from './components/AIAssistant'
 import BusinessIntel from './components/BusinessIntel'
 import { PedidosAdminTab } from './components/Configs'
@@ -282,6 +283,7 @@ function Shell() {
           {tab==='bars'      && <BarsTab/>}
           {tab==='usuarios'  && <UsuariosTab/>}
         {tab==='faturas'   && <Faturas />}
+        {tab==='cashflow'   && <Cashflow />}
         {tab==='bi'        && <BusinessIntel />}
         {tab==='ai'        && <AIAssistant />}
         {tab==='suppliers' && <Fornecedores />}
